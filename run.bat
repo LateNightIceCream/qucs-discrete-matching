0d66fc57654196079a52bd7a91745ef20bae9772
latenighticecream <richardgruenert@live.com>
2022-05-09 09:08:49 +0200

    clean dir

--- run.bat
+++
@@ -1,1 +0,0 @@
-python .\qucs_discrete_matching.py --qucspath "C:\Users\rg\Desktop\qucs-0.0.19-win32-mingw482-asco-freehdl-adms\bin" --nsimulations 100
--- run_two.sh
+++
@@ -1,2 +0,0 @@
-python3 qucs_discrete_matching.py --outfile results/results_0r.txt --templatefile template_0r.sch |& tee results.txt
-python3 qucs_discrete_matching.py --outfile results/results_9pf.txt --templatefile template_9pf.sch |& tee results.txt
